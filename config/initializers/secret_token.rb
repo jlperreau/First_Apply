7a58ab318cd18bb8cd14dbb05d13ae2e43b5133a
jlperreau <jlperreau@nerim.net>
2011-10-09 21:49:33 +0200

    depot Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApply::Application.config.secret_token = '77de80a01576ba97977bacd0108b187c61e3779c6097f55bb112e3376f060e07d39e5640ca5ce508bbce434277b1794bafbaace2fa825fcbff0c66f226499d64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FirstApply::Application.config.session_store :cookie_store, :key => '_First_Apply_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FirstApply::Application.config.session_store :active_record_store
